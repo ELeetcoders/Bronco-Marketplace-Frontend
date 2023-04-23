9b871f7d7bb5e1fd5004e72d59e1cafc2b719e5b
Michael Truong <truong.minh.michael@gmail.com>
2023-04-23 15:30:11 -0700

    testing sign in

---
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Observer } from 'rxjs';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent {
+
+  email: String | null= null;
+  password: String | null= null;
+
+  constructor(private http: HttpClient) {}
+
+  onSignInClick() {
+    const data = {
+      email: this.email,
+      password: this.password
+    };
+  
+    // Send the HTTP POST request to the server
+    const observer: Observer<any> = {
+      next: response => {
+        // Extract the cookie from the response
+        //const cookie = response.headers.get('Set-Cookie');
+        //if (cookie) {
+          // Set the cookie using Document.cookie
+          //document.cookie = cookie;
+        //}
+        console.log(response);
+      },
+      error: error => console.error(error),
+      complete: () => console.log('complete')
+    };
+
+    const options = { withCredentials: true };
+
+    this.http.post('http://api.broncomarketplace.com/login/sign-in', data, options).subscribe(observer);
+  }
+}
